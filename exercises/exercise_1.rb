95db14caa587055d49fe4e6778b309c40057c545
Mia Dinh <miadinh38@gmail.com>
2024-02-21 15:52:17 -0500

    Fix typo of a store's name

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -13,7 +13,7 @@ puts "----------"
 
 
 Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-Store.create(name: 'Richmon', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 
